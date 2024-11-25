a62f332608ecdd5f27df9deca515689b718aed2e
luneto10 <lguedesdecarvalhon2@huskers.unl.edu>
2024-11-24 20:47:23 -0600

    Fix responsiviness

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Weekdays from "./components/Weekdays";
 
 function App() {
     return (
-        <div className="App d-flex align-items-center justify-content-center min-vh-100">
+        <div className="App d-flex align-items-center justify-content-center">
             <div className="auto-width text-white border rounded-2 p-4 d-flex flex-column flex-sm-row align-items-center m-5">
                 <div className="mb-3 mb-md-0 me-0 me-md-3">
                     <Weekdays />

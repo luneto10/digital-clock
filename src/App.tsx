843402d3fe14b655a9c0bf782ee02ad41e1bfef1
luneto10 <lguedesdecarvalhon2@huskers.unl.edu>
2024-11-24 20:39:18 -0600

    Large view

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Weekdays from "./components/Weekdays";
 function App() {
     return (
         <div className="App d-flex align-items-center justify-content-center min-vh-100">
-            <div className="auto-width text-white border rounded-2 p-4 d-flex flex-column flex-md-row align-items-center m-5">
+            <div className="auto-width text-white border rounded-2 p-4 d-flex flex-column flex-sm-row align-items-center m-5">
                 <div className="mb-3 mb-md-0 me-0 me-md-3">
                     <Weekdays />
                 </div>
